e09a571d51db09c39776680d0983855dfafdb105
Aalltrac <124187082+Aalltrac@users.noreply.github.com>
2026-05-02 23:05:35 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,30 @@
-import { BrowserRouter, Routes, Route, Navigate } from \"react-router-dom\";
-import \"./App.css\";
-import { AuthProvider } from \"./contexts/AuthContext\";
-import GlobalBackground from \"./components/three/GlobalBackground\";
-import ProtectedRoute from \"./components/ProtectedRoute\";
-import Layout from \"./components/Layout\";
-import Login from \"./pages/Login\";
-import ProfileSetup from \"./pages/ProfileSetup\";
-import Home from \"./pages/Home\";
-import Planning from \"./pages/Planning\";
-import Disponibilite from \"./pages/Disponibilite\";
-import EvaPass from \"./pages/EvaPass\";
-import Discussion from \"./pages/Discussion\";
-import Strategie from \"./pages/Strategie\";
-import MapPage from \"./pages/MapPage\";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import GlobalBackground from "./components/three/GlobalBackground";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import ProfileSetup from "./pages/ProfileSetup";
+import Home from "./pages/Home";
+import Planning from "./pages/Planning";
+import Disponibilite from "./pages/Disponibilite";
+import EvaPass from "./pages/EvaPass";
+import Discussion from "./pages/Discussion";
+import Strategie from "./pages/Strategie";
+import MapPage from "./pages/MapPage";
 
 function App() {
   return (
-    <div className=\"App\">
+    <div className="App">
       <GlobalBackground />
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            <Route path=\"/\" element={<Navigate to=\"/login\" replace />} />
-            <Route path=\"/login\" element={<Login />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
             <Route
-              path=\"/profile-setup\"
+              path="/profile-setup"
               element={
                 <ProtectedRoute requireProfile={false}>
                   <ProfileSetup />
@@ -32,7 +32,7 @@ function App() {
               }
             />
             <Route
-              path=\"/app\"
+              path="/app"
               element={
                 <ProtectedRoute>
                   <Layout />
@@ -40,14 +40,14 @@ function App() {
               }
             >
               <Route index element={<Home />} />
-              <Route path=\"planning\" element={<Planning />} />
-              <Route path=\"disponibilite\" element={<Disponibilite />} />
-              <Route path=\"eva-pass\" element={<EvaPass />} />
-              <Route path=\"discussion\" element={<Discussion />} />
-              <Route path=\"strategie\" element={<Strategie />} />
-              <Route path=\"strategie/:mapSlug\" element={<MapPage />} />
+              <Route path="planning" element={<Planning />} />
+              <Route path="disponibilite" element={<Disponibilite />} />
+              <Route path="eva-pass" element={<EvaPass />} />
+              <Route path="discussion" element={<Discussion />} />
+              <Route path="strategie" element={<Strategie />} />
+              <Route path="strategie/:mapSlug" element={<MapPage />} />
             </Route>
-            <Route path=\"*\" element={<Navigate to=\"/login\" replace />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
